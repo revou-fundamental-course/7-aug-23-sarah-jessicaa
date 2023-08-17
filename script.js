ab256671d66293f61db2ffe688a447e6b619ad28
sarah-jessicaa <142375997+sarah-jessicaa@users.noreply.github.com>
2023-08-17 10:19:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function hitung() {
+	let sisi1 = parseFloat(document.getElementById("sisi1").value);
+	let sisi2 = parseFloat(document.getElementById("sisi2").value);
+	let sisi3 = parseFloat(document.getElementById("sisi3").value);
+	let alas = parseFloat(document.getElementById("alas").value);
+	let tinggi = parseFloat(document.getElementById("tinggi").value);
+
+	if (sisi1 && sisi2 && sisi3) {
+		let keliling = sisi1 + sisi2 + sisi3;
+		document.getElementById("keliling").innerHTML = keliling.toFixed(2);
+	} else {
+		document.getElementById("keliling").innerHTML = "Masukkan nilai ketiga sisi";
+	}
+
+	if (alas && tinggi) {
+		let luas = (alas * tinggi) / 2;
+		document.getElementById("luas").innerHTML = luas.toFixed(2);
+	} else {
+		document.getElementById("luas").innerHTML = "Masukkan nilai alas dan tinggi";
+	}
+}
